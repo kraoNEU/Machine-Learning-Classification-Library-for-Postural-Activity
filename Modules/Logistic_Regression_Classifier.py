08a834d3277e568d25ac18994ccb5532e154f30d
kraoNEU <chelamkuri.v@northeastern.edu>
2022-04-29 13:08:35 -0400

    Added Tests and Multi Class Tests for LRC

--- Modules/Logistic_Regression_Classifier.py
+++ Modules/Logistic_Regression_Classifier.py
@@ -37,7 +37,9 @@ class BinaryLogisticRegression:
         self.model_bias = 0
 
         for each_iteration in range(self.iteration_count):
-            model_expression = np.dot(input_data, self.model_weights) + self.model_bias
+            vector = np.vectorize(float)
+            x = vector(input_data)
+            model_expression = np.dot(x, self.model_weights) + self.model_bias
             predicted_model_output = self.logistic_function(model_expression)
 
             self.model_weights -= self.learning_rate * self.weights_error_calculator(predicted_model_output)
@@ -50,7 +52,9 @@ class BinaryLogisticRegression:
         param predicted_model_output2: The output predicted by the model being trained
         return: The error of the weights of the model being trained
         """
-        error_in_model_weights = (1 / self.sample_count) * np.dot(self.input_data.T,
+        vector = np.vectorize(float)
+        x1 = vector(self.input_data)
+        error_in_model_weights = (1 / self.sample_count) * np.dot(x1.T,
                                                                   (predicted_model_output2 - self.output_data))
         return error_in_model_weights
 
@@ -74,17 +78,18 @@ class BinaryLogisticRegression:
         """
         return (1 / (1 + np.exp(-this_expression)))
 
-    def predictor_trainor(self, input_data3):
+    def predictor_trainer(self, input_data3):
         """
-
-        :param input_data2:
-        :return:
+        This method is used to calculate the predicted
+        output of the model.
+        param input_data3: the input test data
+        return: the output of the trained model using test data
         """
         model_expression = np.dot(input_data3, self.model_weights) + self.model_bias
         predicted_model_output1 = self.logistic_function(model_expression)
         return predicted_model_output1
 
-    def class_predictor(self, input_data3):
+    def class_predictor(self, input_data2):
         """
         This method predicts the output class for the corresponding
         test input sample by using the trained model.
@@ -92,7 +97,7 @@ class BinaryLogisticRegression:
         return: The output class of the test input sample data
         """
         predicted_model_class = None
-        predicted_model_output = self.predictor_trainor(input_data3)
+        predicted_model_output = self.predictor_trainer(input_data2)
         predicted_model_class = [1 if each_output_sample > 0.5 else 0
                                  for each_output_sample in predicted_model_output]
         return predicted_model_class
@@ -107,4 +112,69 @@ class BinaryLogisticRegression:
         return: The percentage accuracy of the model
         """
         percent_accuracy: int = np.sum(output_model_class == predicted_model_class) / len(output_model_class) * 100
+        return percent_accuracy
+
+
+class MultiClassLogisticRegression:
+    """
+    This class provides library for machine learning classification
+    using Multiclass Logistic Regression Model.
+    """
+
+    def __init__(self):
+        """
+        This method initializes the class of multiclass
+        Logistic Regression.
+        """
+        self.class_model_dict = {}
+
+    def train_model(self, input_data, output_data):
+        """
+        This method helps the model in training using the input and output
+        data from the training dataset.
+        param input_data: The input samples data
+        param output_data: The output data corresponding to the input samples
+        """
+        target_class_list = list(set(output_data))
+        for each_class in target_class_list:
+            new_output_data = []
+            for each_output_sample in output_data:
+                if each_class == each_output_sample:
+                    new_output_data.append(1)
+                else:
+                    new_output_data.append(0)
+            this_model = BinaryLogisticRegression(learning_rate=0.0001, iteration_count=1000)
+            this_model.train_model(input_data, new_output_data)
+            self.class_model_dict[each_class] = this_model
+
+    def class_predictor(self, input_data_new):
+        """
+        This method predicts the output class for the corresponding
+        test input sample by using the trained model.
+        param input_data_new: The test input sample data
+        return: The output class list of the test input sample data
+        """
+        all_classes = self.class_model_dict.keys()
+        predicted_class_list = []
+
+        for each_example in input_data_new:
+            probability_dict = {}
+            for each in all_classes:
+                model = self.class_model_dict[each]
+                n = tuple(model.predictor_trainer(input_data_new))
+                probability_dict[n] = each
+            predicted_class = probability_dict.get(max(probability_dict.keys()))
+            predicted_class_list.append(predicted_class)
+
+        return predicted_class_list
+
+    def accuracy(self, outputClass, predictedClass):
+        """
+        This method calculates the percentage accuracy of the model by comparing
+        the ideal output with the predicted output of the model.
+        param predictedClass: The predicted output of the model
+        param outputClass: The ideal output as per the dataset
+        return: The percentage accuracy of the model
+        """
+        percent_accuracy: int = np.sum(outputClass == predictedClass) / len(outputClass) * 100
         return percent_accuracy
--- Test/Logistic_Regression_Classifier_Test.py
+++ Test/Logistic_Regression_Classifier_Test.py
@@ -1,6 +1,7 @@
+import math
 from sklearn import datasets
-from Modules import Logistic_Regression_Classifier as LRC
 from sklearn.model_selection import train_test_split
+from Modules import Logistic_Regression_Classifier as LRC
 
 test_dataset = datasets.load_breast_cancer()
 test_input_data, test_output_data = test_dataset.data, test_dataset.target
@@ -9,5 +10,15 @@ input_train, input_test, output_train, output_test = train_test_split(test_input
 test_model = LRC.BinaryLogisticRegression(learning_rate=0.0001, iteration_count=1000)
 test_model.train_model(input_train, output_train)
 test_prediction = test_model.class_predictor(input_test)
+blr_accurcy = test_model.percent_accuracy_of_model(output_test, test_prediction)
 print("The percentage accuracy of the binary logistic regression model is: ",
-      test_model.percent_accuracy_of_model(output_test, test_prediction))
+      blr_accurcy)
+assert math.isclose(blr_accurcy, 92.98245614035088)
+
+new_test_model = LRC.MultiClassLogisticRegression()
+new_test_model.train_model(input_train, output_train)
+new_test_prediction = new_test_model.class_predictor(input_test)
+mclr_accurcy = new_test_model.accuracy(output_test, new_test_prediction)
+print("The percentage accuracy of the multi class logistic regression model is: ",
+      mclr_accurcy)
+assert math.isclose(mclr_accurcy, 60.526315789473685)
