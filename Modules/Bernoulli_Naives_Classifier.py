70dacb7db284c8bae5fdd3320d2aa37d6c22361f
kraoNEU <chelamkuri.v@northeastern.edu>
2022-04-29 20:52:15 -0400

    Added Comments to BNC

--- Modules/Bernoulli_Naives_Classifier.py
+++ Modules/Bernoulli_Naives_Classifier.py
@@ -3,8 +3,15 @@ import numpy as np
 
 
 class GaussianNB:
+    """
+    Function: Calculates the Gaussian Naives Bayes Classifier for the Input Dataset
+    Static Methods: 3 Static Methods for Pre-Processing, Splitting and Accuracy Scoring
+    Non-Static Methods: 4 Non-Static Method for fit, posterior calculation, likelihood estimations and predictions
+    Input: Any Binary dataset with independent dataset (preferably)
+    """
 
     def __init__(self):
+        """Initializing the Variables"""
 
         self.features = list
         self.likelihoods = {}
@@ -17,12 +24,19 @@ class GaussianNB:
 
     @staticmethod
     def accuracy_score(y_true, y_pred):
-
+        """
+        function: Static Method for the Calculation of Accuracy for the predicted output
+        returns: Rounded Float Value of Accuracy Result
+        """
         return round(float(sum(y_pred == y_true)) / float(len(y_true)) * 100, 2)
 
     @staticmethod
     def pre_processing(df):
 
+        """
+        function: Static Method for Getting the Target Classes and Input Features
+        returns: Dataframe of Input Variables and Series of Target Classes
+        """
         X = df.drop([df.columns[-1]], axis=1)
         y = df[df.columns[-1]]
 
@@ -30,7 +44,11 @@ class GaussianNB:
 
     @staticmethod
     def train_test_split(x, y, test_train_split=0.25, random_state=None):
-
+        """
+         function: Static Method for the Splitting of Test and Train for the X and y Variables
+         returns: x_train: Input features for training; x_test: Input features for testing
+         y_train; Target class for training; y_test: Target class for Testing
+        """
         x_test = x.sample(frac=test_train_split, random_state=random_state)
         y_test = y[x_test.index]
 
@@ -40,52 +58,83 @@ class GaussianNB:
         return x_train, x_test, y_train, y_test
 
     def fit(self, X, y):
-
+        """
+        function: Tries to fit the Features with the outcome classes of the Training Dataset
+        returns: class posterior calculation and likelihood of the classes
+        """
         self.features = list(X.columns)
         self.X_train = X
         self.y_train = y
         self.train_size = X.shape[0]
         self.num_feats = X.shape[1]
 
+        # Iterates over the features of the dataset
         for feature in self.features:
+
+            # Defining a likelihood dictionary for further use for getting the mean and variance based on the
+            # Input feature
             self.likelihoods[feature] = {}
 
+            # Adding the features to the likelihood and class priors dictionaries to add the mean and variance later
             for outcome in np.unique(self.y_train):
                 self.likelihoods[feature].update({outcome: {}})
                 self.class_priors.update({outcome: 0})
 
+        # Call the Method for Posterior Calculation and Likelihood Calculation
         self._calc_class_prior()
         self._calc_likelihoods()
 
     def _calc_class_prior(self):
+
+        """
+        function: calculates the posterior probability
+        returns: the list of posterior probability outcomes for the given input
+        """
 
         for outcome in np.unique(self.y_train):
             outcome_count = sum(self.y_train == outcome)
             self.class_priors[outcome] = outcome_count / self.train_size
 
     def _calc_likelihoods(self):
-
+        """
+        function: calculates the maximum possible likelihood of the features
+        returns: list of the likelihood based on the mean and variance
+        """
         for feature in self.features:
 
             for outcome in np.unique(self.y_train):
+
+                # Getting the likelihoods of input variable "Mean"
                 self.likelihoods[feature][outcome]['mean'] = self.X_train[feature][
                     self.y_train[self.y_train == outcome].index.values.tolist()].mean()
 
+                # Getting the likelihoods of input variable "Variance"
                 self.likelihoods[feature][outcome]['variance'] = self.X_train[feature][
                     self.y_train[self.y_train == outcome].index.values.tolist()].var()
 
     def predict(self, X):
 
+        """
+        Function: Predicts Whether the Input Features are for a particular class (target Classes) using Likelihood.
+        Returns: The Array of Results for the Likelihood of the Target Classes
+        Citation: https://towardsdatascience.com/naive-bayes-explained-9d2b96f4a9c0
+        Formula and Basic Understanding have been inspired from the citation given above.
+        """
+
         results = []
         X = np.array(X)
 
+        # Iterating thru the Input features of X
         for query in X:
+
+            # Storing the Posterior Outcome
             probs_outcome = {}
 
             for outcome in np.unique(self.y_train):
                 prior = self.class_priors[outcome]
                 likelihood = 1
 
+                # Getting the Mean and Variance for the particular feature feature_outcome
                 for feat, feat_val in zip(self.features, query):
                     mean = self.likelihoods[feat][outcome]['mean']
                     var = self.likelihoods[feat][outcome]['variance']
@@ -94,7 +143,11 @@ class GaussianNB:
                 posterior_numerator = (likelihood * prior)
                 probs_outcome[outcome] = posterior_numerator
 
+            # Getting the final result of the Probability Outcome
             result = max(probs_outcome, key=lambda x: probs_outcome[x])
+
+            # Append the results in the list
+            # Appending the results into the List
             results.append(result)
 
         return np.array(results)
